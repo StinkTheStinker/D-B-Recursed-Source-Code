aff9b09c8602943883bd24f8423e515a89870089
StinkTheStinker <145304742+StinkTheStinker@users.noreply.github.com>
2023-12-24 12:41:58 -0500

    yup

--- Quick SETUP.bat
+++ Quick SETUP.bat
@@ -1,8 +1,8 @@
 @echo off
-echo Quickly setting up JS Engine for compiling...
+echo Quickly setting up the game for compiling...
 haxelib setup C:/haxelib
 haxelib --global install hmm
 haxelib --global run hmm install
-echo Finished. You may now compile JS Engine!
+echo Finished. You may now compile the game! Wohoo!
 echo Press any key to exit.
 pause >nul
