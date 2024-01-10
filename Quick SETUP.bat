5171c43aa39e283fa653061237750aae3cc7d257
StinkTheStinker <145304742+StinkTheStinker@users.noreply.github.com>
2024-01-09 23:02:52 -0500

    Source Code Deletion Read "README.md"

--- Quick SETUP.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Quickly setting up the game for compiling...
-haxelib setup C:/haxelib
-haxelib --global install hmm
-haxelib --global run hmm install
-echo Finished. You may now compile the game! Wohoo!
-echo Press any key to exit.
-pause >nul
